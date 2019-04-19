5068e63721dea6107ea2612f2decbcffc85c8655
tactnician <tactnician@gmail.com>
2019-04-19 12:34:57 -0400

    created testing scaffold

---
+++ __test__/router.spec.js
@@ -0,0 +1,32 @@
+const router = require('../config/routes');
+const request = require('supertest');
+
+describe('Routes', () => {
+    
+    describe('POST /games', () => {
+        it('should return status 201 if successful or 422 if fails', async () => {
+            
+        })
+        it('should insert the provided game', async () => {
+            
+        })
+        it('should return an id', async () => {
+            
+        })
+    })
+    
+    describe('GET /games', () => {
+        it('should return status 200', (done) => {
+            
+        })
+        it('should return an array', (done) => {
+            
+        })
+        it('should return JSON', (done) => {
+            
+        })
+    })
+    
+    
+
+})
